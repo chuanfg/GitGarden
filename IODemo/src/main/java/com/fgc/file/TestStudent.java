3b8f5a05836530c3732f6eedc4dfc51b50c14fae
fengguochuan <1182905535@qq.com>
2022-12-11 17:02:27 +0800

    modify TestStudent

--- IODemo/src/main/java/com/fgc/file/TestStudent.java
+++ IODemo/src/main/java/com/fgc/file/TestStudent.java
@@ -3,6 +3,6 @@ package com.fgc.file;
 public class TestStudent {
     public static void main(String[] args) {
         Student student=new Student();
-
+        student.setName("zhangsan");
     }
 }
